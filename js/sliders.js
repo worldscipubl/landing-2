359e3f607f15421a08c8260eb7f034e05313cd86
Igor Tyugaev <igor.tyugaev@gmail.com>
2021-07-25 15:08:11 +0500

    Add adaptive advantages

---
+++ js/sliders.js
@@ -0,0 +1,14 @@
+const sliders = document.querySelectorAll('.js-slider');
+sliders.forEach((slider) => {
+    initSlider(slider);
+})
+
+function initSlider(slider) {
+    const flktySlider = new Flickity(slider, {
+        cellAlign: 'left',
+        prevNextButtons: false,
+        autoPlay: 5000,
+        wrapAround: true,
+        watchCSS: true
+    });
+}
