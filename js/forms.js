978c87e4d1594c6598db76bde290a9d93c070630
Igor Tyugaev <igor.tyugaev@gmail.com>
2021-09-27 14:14:24 +0500

    fix new link rounting and snakebar

--- js/forms.js
+++ js/forms.js
@@ -485,14 +485,14 @@
 
     const showSnakebar = (msg) => {
         const snackbar = document.getElementById("snackbar");
-        const html = `<div id="snackbar">${msg}</div>`;
-
         // Add the "show" class to DIV
-        snackbar.className = "show";
+        snackbar.classList.add('show');
+        snackbar.innerText = msg || ""
         // After 3 seconds, remove the show class from DIV
-        setTimeout(() => { snackbar.className = x.className.replace("show", ""); snackbar.remove}, 3000);
+        setTimeout(() => {
+            snackbar.classList.remove('show')
+        }, 3000);
     }
-
     const forms = document.querySelectorAll('form');
     forms.forEach((_form) => {
         if (!_form.hasAttribute("data-form-type"))
